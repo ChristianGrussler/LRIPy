ea055fbde832942990c77e04e9eea9f11e84afc9
Christian Grussler <36197598+LowRankOpt@users.noreply.github.com>
2018-08-13 16:49:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='lripy',
-    version='0.0.2',
+    version='0.0.3',
     author='Christian Grussler',
     author_email='christian.grussler@eng.cam.ac.uk',
     zip_safe=False,
